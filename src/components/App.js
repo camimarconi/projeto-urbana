4a3ab2aa54bc2d3c94f416cd95678ec931e9f1ff
Camila Solér <msoler.camila@gmail.com>
2023-01-18 17:54:21 -0300

    estruturação básica

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import NotFound from "../pages/NotFound";
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
