a9de684bdbca5851315a44fcb265c40c77a7a94c
Camila Solér <msoler.camila@gmail.com>
2023-02-05 18:09:07 -0300

    hover arrumado serviços - cor trocando

--- src/components/ServiçosModel.js
+++ src/components/ServiçosModel.js
@@ -5,6 +5,10 @@ import iconConsultB from "../assets/images/icon-consult-b.png";
 import iconpesqB from "../assets/images/icon-pesq-b.png";
 import icontreinamtB from "../assets/images/icon-treinam-b.png";
 import iconcolabtB from "../assets/images/icon-colab-b.png";
+import iconConsultV from "../assets/images/1-icon-consult-vinho.png";
+import iconpesqV from "../assets/images/2-icon-pesq-vinho.png";
+import icontreinamtV from "../assets/images/icon-treinam-vinho.png";
+import iconcolabtV from "../assets/images/icon-colab-vinho.png";
 
 function ServiçosModel() {
   return (
@@ -18,7 +22,12 @@ function ServiçosModel() {
             <Link className="link-text-serv" to="/consultoria">
               <img
                 src={iconConsultB}
-                className="card-img-top"
+                className="card-img-top-bege"
+                alt="icon-consultoria"
+              />
+              <img
+                src={iconConsultV}
+                className="card-img-top-vinho"
                 alt="icon-consultoria"
               />
               <h5 className="mt-4 col titleBelow">Consultoria e gestão</h5>
@@ -33,7 +42,12 @@ function ServiçosModel() {
             <Link className="link-text-serv" to="/pesquisa">
               <img
                 src={iconpesqB}
-                className="card-img-top"
+                className="card-img-top-bege"
+                alt="icon-pesquisa"
+              />
+              <img
+                src={iconpesqV}
+                className="card-img-top-vinho"
                 alt="icon-pesquisa"
               />
               <h5 className="mt-4 col titleBelow">Pesquisa e produção</h5>
@@ -48,7 +62,12 @@ function ServiçosModel() {
             <Link className="link-text-serv" to="/treinamento">
               <img
                 src={icontreinamtB}
-                className="card-img-top"
+                className="card-img-top-bege"
+                alt="icon-treinamento"
+              />
+              <img
+                src={icontreinamtV}
+                className="card-img-top-vinho"
                 alt="icon-treinamento"
               />
               <h5 className="mt-4 col titleBelow">
@@ -65,7 +84,12 @@ function ServiçosModel() {
             <Link className="link-text-serv" to="/colaboração">
               <img
                 src={iconcolabtB}
-                className="card-img-top"
+                className="card-img-top-bege"
+                alt="icon-colaboração"
+              />
+              <img
+                src={iconcolabtV}
+                className="card-img-top-vinho"
                 alt="icon-colaboração"
               />
               <h5 className="mt-4 col titleBelow">
